5a45c0bb8f44ef5e2e3bd95c86880715e83a759e
Gregory Power <31050507+gregorywaynepower@users.noreply.github.com>
2026-06-11 23:40:03 -0400

    reformat

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,7 @@
 
 {
 
-  nixpkgs.config.permittedInsecurePackages = ["electron-39.8.10"];
+  nixpkgs.config.permittedInsecurePackages = [ "electron-39.8.10" ];
 
   nix.settings.experimental-features = [
     "nix-command"
