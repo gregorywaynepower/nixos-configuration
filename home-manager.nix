6b0069596ccc9a0087fb9dd1a923dedfdc3371ab
Gregory Power <31050507+gregorywaynepower@users.noreply.github.com>
2026-06-15 20:55:28 -0400

    reformat home-manager.nix

--- home-manager.nix
+++ home-manager.nix
@@ -1,14 +1,21 @@
-{ inputs, ... }:
+{ inputs, config, ... }:
 {
   imports = [ inputs.home-manager.nixosModules.home-manager ];
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-  home-manager.extraSpecialArgs = { inherit inputs; };
-  home-manager.users.gregorywpower = {
-    programs = {
-      zellij = {
-        enable = true;
-        enableBashIntegration = true;
+  home-manager = {
+    useGlobalPkgs = true;
+    useUserPackages = true;
+    extraSpecialArgs = { inherit inputs; };
+    backupFileExtension = "backup";
+    overwriteBackup = true;
+    users = {
+      gregorywpower = {
+        home.stateVersion = config.system.stateVersion;
+        programs = {
+          zellij = {
+            enable = true;
+            enableBashIntegration = true;
+          };
+        };
       };
     };
   };
