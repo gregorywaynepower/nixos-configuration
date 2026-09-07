33d782c449c8f5b9bc0df7c85b9a3ff0f4381494
Gregory Power <31050507+gregorywaynepower@users.noreply.github.com>
2026-09-07 15:02:37 -0400

    initial tmux configuration

--- home-manager.nix
+++ home-manager.nix
@@ -29,7 +29,30 @@
             mouse = true;
             clock24 = true;
             keyMode = "vi";
-          };
+            extraConfig = ''
+              # Split panes with | and -
+              bind | split-window -h
+              bind - split-window -v
+              # Move between panes with Prefix h,j,k,l​
+              bind h select-pane -L
+              bind j select-pane -D
+              bind k select-pane -U
+              bind l select-pane -R
+              # Pane resizing with Prefix H, J, K, L
+              bind -r H resize-pane -L 5
+              bind -r J resize-pane -D 5
+              bind -r K resize-pane -U 5
+              bind -r L resize-pane -R 5
+              # Quick window selection
+              bind -r C-h select-window -t :-
+              bind -r C-l select-window -t :+
+              # Set default terminal mode to 256color mode
+              set -g default-terminal "tmux-256color"
+              # Enable activity alerts
+              set -w -g monitor-activity on
+              set -w -g visual-activity on
+             '';
+            };
           zellij = {
             enable = true;
             settings = {
